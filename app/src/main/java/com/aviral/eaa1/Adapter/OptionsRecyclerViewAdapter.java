22e908a8e2ab97ef5787d6c229e5ef39fdd6d176
Aviral <aviral.apphub@gmail.com>
2023-04-04 09:38:15 +0530

    Added Source Code of Main Activity!

---
+++ app/src/main/java/com/aviral/eaa1/Adapter/OptionsRecyclerViewAdapter.java
@@ -0,0 +1,97 @@
+package com.aviral.eaa1.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.aviral.eaa1.Models.Options;
+import com.aviral.eaa1.R;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class OptionsRecyclerViewAdapter
+        extends RecyclerView.Adapter<OptionsRecyclerViewAdapter.ViewHolder> {
+
+    private ArrayList<Options> optionList;
+
+    public OptionsRecyclerViewAdapter(ArrayList<Options> optionList) {
+        this.optionList = optionList;
+    }
+
+    @NonNull
+    @Override
+    public OptionsRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.snippet_layout_options,
+                parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OptionsRecyclerViewAdapter.ViewHolder holder, int position) {
+
+        setAnimation(holder.itemView, holder.itemView.getContext());
+
+        if (optionList.get(position).getChances() <= 0) {
+            holder.chances.setTextColor(holder.itemView.getContext()
+                    .getResources().getColor(R.color.chances_not_left));
+
+            holder.optionButton.setBackground(AppCompatResources.getDrawable(
+                    holder.itemView.getContext(),
+                    R.drawable.option_price_button_backround_grey));
+
+            holder.chances.setText(R.string.reward_completed);
+
+        }  else {
+            holder.chances.setText(optionList.get(position).getChances() + " Chance left");
+        }
+
+        holder.optionTitle.setText(optionList.get(position).getOptionDetails());
+        holder.optionButton.setText(optionList.get(position).getOptionBalance());
+
+        Glide.with(holder.itemView.getContext())
+                .load(optionList.get(position).getOptionImage())
+                .into(holder.optionImage);
+    }
+
+    private void setAnimation(View itemView, Context context) {
+        Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
+
+        itemView.startAnimation(animation);
+    }
+
+    @Override
+    public int getItemCount() {
+        return optionList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView optionImage;
+        private final TextView optionTitle;
+        private final TextView chances;
+        private final AppCompatButton optionButton;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            optionImage = itemView.findViewById(R.id.option_image);
+            optionTitle = itemView.findViewById(R.id.option_title);
+            chances = itemView.findViewById(R.id.chances);
+            optionButton = itemView.findViewById(R.id.price_button);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/aviral/eaa1/MainActivity.java
@@ -0,0 +1,157 @@
+package com.aviral.eaa1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.aviral.eaa1.Adapter.CategoryRecyclerViewAdapter;
+import com.aviral.eaa1.Adapter.OptionsRecyclerViewAdapter;
+import com.aviral.eaa1.Models.Category;
+import com.aviral.eaa1.Models.Options;
+import com.aviral.eaa1.Utils.RecyclerViewMargin;
+import com.aviral.eaa1.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setUpCategoryAdapter();
+        setUpOptionAdapter();
+//        setUpBottomNavigation();
+
+    }
+
+
+
+    private void setUpOptionAdapter() {
+        ArrayList<Options> optionsArrayList = new ArrayList<>();
+
+        optionsArrayList.add(new Options(
+                R.drawable.ic_rate,
+                "Rate 5 Star & Earn Money",
+                1,
+                "₹10.00"
+        ));
+
+        optionsArrayList.add(new Options(
+                R.drawable.ic_daily,
+                "Daily Bonus",
+                4,
+                "₹0.25"
+        ));
+
+        optionsArrayList.add(new Options(
+                R.drawable.ic_collect,
+                "Collect Rewards",
+                0,
+                "₹0.20"
+        ));
+
+        optionsArrayList.add(new Options(
+                R.drawable.ic_watch_videos,
+                "Watch Videos",
+                10,
+                "₹0.50"
+        ));
+
+        optionsArrayList.add(new Options(
+                R.drawable.ic_gold_points,
+                "Gold Points",
+                1,
+                "₹0.50"
+        ));
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
+
+        binding.optionsRecyclerView.setLayoutManager(linearLayoutManager);
+
+        OptionsRecyclerViewAdapter optionsRecyclerViewAdapter =
+                new OptionsRecyclerViewAdapter(optionsArrayList);
+
+        RecyclerViewMargin recyclerViewMargin = new RecyclerViewMargin(3);
+        binding.optionsRecyclerView.addItemDecoration(recyclerViewMargin);
+
+        binding.optionsRecyclerView.setAdapter(optionsRecyclerViewAdapter);
+
+    }
+
+    private void setUpCategoryAdapter() {
+        ArrayList<Category> categoryList = new ArrayList<>();
+
+        categoryList.add(new Category(
+                R.drawable.ic_ludo,
+                "Ludo"
+        ));
+
+        categoryList.add(new Category(
+                R.drawable.ic_chess,
+                "Chess"
+        ));
+
+        categoryList.add(new Category(
+                R.drawable.ic_pubg,
+                "Pubg"
+        ));
+
+        categoryList.add(new Category(
+                R.drawable.ic_puzzle,
+                "Puzzle"
+        ));
+
+        categoryList.add(new Category(
+                R.drawable.ic_card,
+                "Card"
+        ));
+
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,
+                LinearLayoutManager.HORIZONTAL, false);
+
+        binding.categoriesRecyclerView.setLayoutManager(linearLayoutManager);
+
+        CategoryRecyclerViewAdapter categoryRecyclerViewAdapter =
+                new CategoryRecyclerViewAdapter(categoryList);
+
+        RecyclerViewMargin recyclerViewMargin = new RecyclerViewMargin(5);
+        binding.categoriesRecyclerView.addItemDecoration(recyclerViewMargin);
+
+        binding.categoriesRecyclerView.setAdapter(categoryRecyclerViewAdapter);
+
+    }
+
+//    public void setUpBottomNavigation() {
+//        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+//
+//        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+//            switch (item.getItemId()) {
+//                case R.id.earn_money:
+//                    // Do something when the "Home" item is clicked
+//                    return true;
+//                case R.id.spin:
+//                    // Do something when the "Search" item is clicked
+//                    return true;
+//                case R.id.scratch_card:
+//                    // Do something when the "Favorites" item is clicked
+//                    return true;
+//                case R.id.others:
+//                    // Do something when the "Notifications" item is clicked
+//                    return true;
+//                case R.id.profile:
+//                    // Do something when the "Profile" item is clicked
+//                    return true;
+//                default:
+//                    return false;
+//            }
+//        });
+//    }
+}
